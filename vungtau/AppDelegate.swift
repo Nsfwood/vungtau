e5ec121529c9356b9b6bf9d69fe104e9f0cfe688
Alexander Rohrig <me@alexanderrohrig.com>
2020-10-12 05:46:25 -0700

    Initial Commit

---
+++ vungtau/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  vungtau
+//
+//  Created by Alexander Rohrig on 10/12/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ vungtau/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  vungtau
+//
+//  Created by Alexander Rohrig on 10/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
